0bf1992fc009e54e09cdf795a019ff092ab58c01
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-10-23 19:14:13 +0300

    fix: Временный фикс мобильного меню теперь при скроле не видно контента вверху. но все равно какая-то херня, пока не знаю, как оно должно работать

--- scripts/mobileMenu.js
+++ scripts/mobileMenu.js
@@ -4,9 +4,11 @@ const headerMenu = document.querySelector('.header__menu');
 
 mobileMenuBtn.addEventListener('click', () => {
   if(!mobileMenuBtnBurgerEl.classList.contains('menu-button__burger_opened')) {
+    mobileMenuBtn.classList.add('menu-button_opened');
     mobileMenuBtnBurgerEl.classList.add('menu-button__burger_opened');
     headerMenu.classList.add('header__menu_opened');
   } else {
+    mobileMenuBtn.classList.remove('menu-button_opened');
     mobileMenuBtnBurgerEl.classList.remove('menu-button__burger_opened');
     headerMenu.classList.remove('header__menu_opened');
   }
