21177dc9adcd8f329ce916e79e082a038f142c54
Leth4L <carribean_08@mail.ru>
2022-10-26 14:54:44 +0300

    добавил горизонатальный скролл блоку с пьесами

--- pages/index.js
+++ pages/index.js
@@ -4,4 +4,11 @@ const peopleScrolling = new HorizontalScrollSection(
   '.people-section__peoples',
   'box-for-horizontal-scroll'
 );
+
+const playSectionScrolling = new HorizontalScrollSection(
+  '.play-section__wrapper',
+  'box-for-horizontal-scroll'
+);
+
 peopleScrolling.setScrollable();
+playSectionScrolling.setScrollable();
